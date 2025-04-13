43d0d80d128a0aafc01d511becdf428404ea4a45
Lih Ling Yen <lihling.yen@ideagenplc.com>
2025-04-13 23:40:54 +0800

    EM-B-1 - first checkin.

---
+++ DataLayer/ExpensesManagementContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelLayer;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ExpensesManagementContext : DbContext 
+    {
+        private const string MYCONN = "server=127.0.0.1; port=3333; uid=root; pwd=root; database=ExpensesManagement;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var mySQVersion = new MySqlServerVersion(new Version(10, 4, 17));
+            optionsBuilder.UseMySql(MYCONN, mySQVersion); 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonalModel>() 
+                .HasMany(p => p.incomes) 
+                .WithOne(i => i. personal)    
+                .HasForeignKey(i => i.personalId);
+
+            modelBuilder.Entity<PersonalModel>()
+                .HasMany(p => p.expenses)
+                .WithOne(e => e.personal)
+                .HasForeignKey(e => e.personalId);
+        }
+
+        public DbSet<PersonalModel> personal { get; set; }
+        public DbSet<IncomeModel> income { get; set; }
+        public DbSet<ExpenseModel> expense { get; set; }
+
+    }
+}
---
+++ WepAPI/Controllers/ExpensesManagementController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessInterfaceLayer;
+using BusinessLogicLayer;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+
+namespace WepAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpensesManagementController : Controller
+    {
+        private readonly IExpenseIncomeRepository _iIncomeExpenseRepository;
+
+        public ExpensesManagementController(IExpenseIncomeRepository iIncomeExpenseRepository)
+        {
+            _iIncomeExpenseRepository = iIncomeExpenseRepository;
+        }
+
+        [HttpGet("RetrieveIncomesExpenses")]
+        public async Task<List<DataGridModel>> RetrieveIncomesExpenses()
+        {
+            try
+            {
+                return await _iIncomeExpenseRepository.RetrieveIncomeExpenseTable();
+            }
+            catch (Exception ex) 
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost("AddOrExpensesIncomes")]
+        public async Task<IActionResult> AddOrUpdateExpensesIncomes(ExpensesIncomesModel expensesIncomes)
+        {
+            if (expensesIncomes == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var response = await _iIncomeExpenseRepository.AddorUpdateIncomesExpenses(expensesIncomes);
+                return Ok(response);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
